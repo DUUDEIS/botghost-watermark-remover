2e4ce243b96d89c50e815ac93e5f0cbe3a7d46e9
DUUDEIS <134599764+DUUDEIS@users.noreply.github.com>
2024-02-09 06:12:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-    await bot.change_presence(activity=discord.Streaming(name='streaming-name', url='https://www.twitch.tv/urtwitchusername'))
+    await bot.change_presence(activity=discord.Watching(name='NEX HOSTHING'))
 
-bot.run('YOUR_BOT_TOKEN')
+bot.run('MTA2NjAyMjk0ODExMDA4MjE0OQ.GYVeQo.vLZig9ydUlsb5iuNMJcuadYPoGnnEMLUHa-K9o')
